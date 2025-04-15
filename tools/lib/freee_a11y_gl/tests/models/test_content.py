9c7e6869a24bf9ad9ccda0ec0d5bd1043dba1675
Masafumi NAKANE <max@freee.co.jp>
2025-04-15 16:27:43 +0900

    More tests for the Guideline class.

--- tools/lib/freee_a11y_gl/tests/models/test_content.py
+++ tools/lib/freee_a11y_gl/tests/models/test_content.py
@@ -1,11 +1,107 @@
 import pytest
 from pathlib import Path
 from freee_a11y_gl.models.content import Guideline
+from freee_a11y_gl.relationship_manager import RelationshipManager
+from freee_a11y_gl.models.faq.article import Faq
 
 class TestGuideline:
-    def test_template_data(self, guideline_factory):
+    def test_init(self, all_check_data, setup_categories, setup_wcag_sc):
+        """Test initialization of the Guideline class."""
+        rel = RelationshipManager()
+        guideline_data = {
+            "id": "guideline1",
+            "sortKey": "1",
+            "category": "markup",
+            "title": {"ja": "ガイドラインタイトル", "en": "Guideline Title"},
+            "guideline": {"ja": "ガイドラインの内容", "en": "Guideline content"},
+            "intent": {"ja": "ガイドラインの意図", "en": "Guideline intent"},
+            "platform": ["web", "mobile"],
+            "checks": ["0151", "0171"],
+            "sc": ["1.1.1", "2.1.2"],
+            "info": ["exp-markup-semantics", "exp-link-tab-order"],
+            "src_path": "path/to/guideline.yaml"
+        }
+        guideline = Guideline(guideline_data)
+        assert guideline.id == "guideline1"
+        assert guideline.sort_key == "1"
+        assert guideline.data.title == {"ja": "ガイドラインタイトル", "en": "Guideline Title"}
+        assert guideline.data.guideline == {"ja": "ガイドラインの内容", "en": "Guideline content"}
+        assert guideline.data.intent == {"ja": "ガイドラインの意図", "en": "Guideline intent"}
+        assert guideline.data.platform == ["web", "mobile"]
+        assert guideline.src_path == "path/to/guideline.yaml"
+        assert guideline.object_type == "guideline"
+        assert guideline.sort_key == "1"
+
+        category = rel.get_related_objects(guideline, "category")[0]
+        assert category.names == {"ja": "マークアップと実装", "en": "Markup and Implementation"}
+
+        scs = rel.get_related_objects(guideline, "wcag_sc")
+        assert len(scs) == 2
+        assert set(guideline_data["sc"]) == set([sc.id for sc in scs])
+
+        inforefs = rel.get_related_objects(guideline, "info_ref")
+        assert len(inforefs) == 2
+        assert set(guideline_data["info"]) == set([info.ref for info in inforefs])
+
+    def test_duplicate_id(self, all_check_data, setup_categories, setup_wcag_sc):
+        """Test duplicate ID handling."""
+        guideline_data = {
+            "id": "guideline1",
+            "sortKey": "1",
+            "category": "markup",
+            "title": {"ja": "ガイドラインタイトル", "en": "Guideline Title"},
+            "guideline": {"ja": "ガイドラインの内容", "en": "Guideline content"},
+            "intent": {"ja": "ガイドラインの意図", "en": "Guideline intent"},
+            "platform": ["web", "mobile"],
+            "checks": ["0151", "0171"],
+            "sc": ["1.1.1", "2.1.2"],
+            "info": ["exp-markup-semantics", "exp-link-tab-order"],
+            "src_path": "path/to/guideline.yaml"
+        }
+        Guideline(guideline_data)
+        with pytest.raises(ValueError, match="Duplicate guideline ID: guideline1"):
+            Guideline(guideline_data)
+
+    def test_duplicate_sortkey(self, all_check_data, setup_categories, setup_wcag_sc):
+        """Test duplicate sort key handling."""
+        guideline_data1 = {
+            "id": "guideline1",
+            "sortKey": "1",
+            "category": "markup",
+            "title": {"ja": "ガイドラインタイトル", "en": "Guideline Title"},
+            "guideline": {"ja": "ガイドラインの内容", "en": "Guideline content"},
+            "intent": {"ja": "ガイドラインの意図", "en": "Guideline intent"},
+            "platform": ["web", "mobile"],
+            "checks": ["0151", "0171"],
+            "sc": ["1.1.1", "2.1.2"],
+            "info": ["exp-markup-semantics", "exp-link-tab-order"],
+            "src_path": "path/to/guideline.yaml"
+        }
+        guideline_data2 = {
+            **guideline_data1,
+            **{"id": "guideline2", "sortKey": "1"}
+        }
+        Guideline(guideline_data1)
+        with pytest.raises(ValueError, match="Duplicate guideline sortKey: 1"):
+            Guideline(guideline_data2)
+
+    def test_template_data(self, guideline_factory, setup_faq_tags):
         """Test generating template data."""
         sample_guideline = guideline_factory("markup/semantics")
+        mock_faq = {
+            "id": "faq1",
+            "object_type": "faq",
+            "sortKey": "1",
+            "src_path": "path/to/faq1.yaml",
+            "updated": "2023-10-01",
+            "title": {"ja": "FAQタイトル", "en": "FAQ Title"},
+            "problem": {"ja": "問題", "en": "Problem"},
+            "solution": {"ja": "解決策", "en": "Solution"},
+            "explanation": {"ja": "説明", "en": "explanation"},
+            "guidelines": ["gl-markup-semantics"],
+            "tags": ["axe"]
+        }
+        Faq(mock_faq)
         template_data = sample_guideline.template_data("ja")
         expected_template_data = {
             'id': 'gl-markup-semantics',
@@ -198,8 +294,13 @@ class TestGuideline:
             ],
             'info': [
                 ':ref:`exp-markup-semantics`'
-            ]
+            ],
+            "faqs": ["faq1"]
         }
+        check_keys = ["id", "title", "platform", "guideline", "intent", "category", "checks", "scs", "info", "faqs"]
+        for key in check_keys:
+            assert key in template_data
+            assert template_data[key] == expected_template_data[key]
         assert template_data == expected_template_data
 
     def test_list_all_src_paths(self, guideline_factory, sample_dir):
@@ -211,3 +312,32 @@ class TestGuideline:
         src_paths = Guideline.list_all_src_paths()
         assert len(src_paths) == len(expected_src_paths)
         assert set(expected_src_paths) == set(src_paths)
+
+    def test_get_category_and_id(self, guideline_factory):
+        """Test getting category and ID."""
+        sample_guideline = guideline_factory("text/heading-label")
+        category_data = sample_guideline.get_category_and_id("ja")
+        assert category_data["category"] == "テキスト"
+        assert category_data["guideline"] == "gl-text-heading-label"
+
+    def test_link_data(self, guideline_factory):
+        """Test generating link data."""
+        sample_guideline = guideline_factory("form/tab-order")
+        baseurl = "https://example.com"
+        rel = RelationshipManager()
+        category = rel.get_related_objects(sample_guideline, "category")[0]
+        category_id = category.id
+        category_name_ja = category.get_name("ja")
+        category_name_en = category.get_name("en")
+        expected_link_data = {
+            "text": {
+                "ja": f"{category_name_ja}：{sample_guideline.data.title["ja"]}",
+                "en": f"{category_name_en}: {sample_guideline.data.title["en"]}"
+            },
+            "url": {
+                "ja": f"{baseurl}/categories/{category_id}.html#{sample_guideline.id}",
+                "en": f"{baseurl}/en/categories/{category_id}.html#{sample_guideline.id}"
+            }
+        }
+        link_data = sample_guideline.link_data(baseurl)
+        assert link_data == expected_link_data
